9a87863100973e05f220e051b2868a13488df1dd
ketace06 <ismail.omaramin06@gmail.com>
2025-06-16 10:05:50 +0200

    feat: Add an SQL script that allows to recreate a db directly from SQL

---
+++ database/create_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE "posts" (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    image TEXT,
+    content TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
