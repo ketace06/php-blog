744d8052ace11c9f9d5796187f6b18054f7d77aa
ketace06 <ismail.omaramin06@gmail.com>
2025-06-17 14:18:28 +0200

    feat: protect pages from unauthenticated access
    closes: #5

--- public/includes/navbar.php
+++ public/includes/navbar.php
@@ -1,9 +1,30 @@
+<?php
+session_start();
+$isLoggedIn = isset($_SESSION['user_id']) && isset($_SESSION['username']);
+?>
+
 <nav>
     <a href="/" class="site-title">The daily loot</a>
     <div class="nav-buttons">
-        <a href="post-creation.php">Start writing</a>
-        <a href="login-page.php">Log in</a>
-        <a href="logout.php">Log out</a>
-        <a href="signup-page.php">Sign up</a>
+        <?php if ($isLoggedIn) : ?>
+            <div class="welcome-message">
+                <a href="post-creation.php">Start writing</a>
+            </div>
+            <a href="logout.php">Log out</a>
+        <?php else: ?>
+            <?php
+            $currentPage = basename($_SERVER['PHP_SELF']);
+            if (
+                $currentPage === 'post-creation.php' ||
+                $currentPage === 'post-edition.php' ||
+                $currentPage === 'logout.php'
+            ) {
+                header('Location: login-page.php');
+                exit();
+            }
+            ?>
+            <a href="login-page.php">Log in</a>
+            <a href="signup-page.php">Sign up</a>
+        <?php endif; ?>
     </div>
 </nav>
